a62c9fe9bd0f78c716e7c03c9dd122b050c37df4
= <=>
2023-02-22 14:19:46 +0100

    memory board styled

--- src/components/ChampionsMemory.tsx
+++ src/components/ChampionsMemory.tsx
@@ -131,7 +131,7 @@ localStorage.clear()
   };
   return (
     <div id="MemoryGame">
-      {level.current === 1 && (
+      {level.current === 5 && (
         <Modal
           toggleModalOn={toggleModalOn}
           buttonText="Would you like to play again?"
