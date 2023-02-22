10e32bacd4502a5542b902cf42889f8749d6b0b2
= <=>
2023-02-22 12:44:08 +0100

    Homepage customized

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -19,40 +19,46 @@ interface iGamesPlayed {
 }
 
 export default function Homepage() {
-  const [gamesPlayed, setGamesPlayed] = useState<iGamesPlayed>({});
+  const [gamesPlayed, setGamesPlayed] = useState<iGamesPlayed>();
   useEffect(() => {
     const ls = localStorage.getItem("gamesPlayed");
     if (ls) {
       setGamesPlayed(JSON.parse(ls));
     }
   }, []);
+  console.log(gamesPlayed);
+
   return (
     <div id="homepage-container">
       <Header id="homepage-header">
         <Title title="World Cup Champions Games" />
       </Header>
       <main id="games-container">
-        <div className="game-container">
+        <div
+          className={`game-container ${
+            !!gamesPlayed ? "games-played-container" : ""
+          }`}
+        >
           <div
             className={`game-link-box ${
-              !!gamesPlayed.guessChampion ? "game-played" : ""
+              !!gamesPlayed?.guessChampion ? "game-played" : ""
             }`}
           >
             <Link to="/guess-your-champion">Guess you champion</Link>
           </div>
-          {!!gamesPlayed.guessChampion?.bestResult && (
+          {!!gamesPlayed?.guessChampion?.bestResult && (
             <div className="game-results">
               <h2>
-                {gamesPlayed.guessChampion?.gameFinished
+                {gamesPlayed?.guessChampion?.gameFinished
                   ? "You've already won this game!"
                   : "Try again to win this game"}
               </h2>
 
-              {gamesPlayed.guessChampion?.bestResult && (
+              {gamesPlayed?.guessChampion?.bestResult && (
                 <h3>
-                  `You guessed{" "}
-                  <span>{gamesPlayed.guessChampion?.bestResult}</span>{" "}
-                  {gamesPlayed.guessChampion?.bestResult === 1
+                  You guessed{" "}
+                  <span>{gamesPlayed?.guessChampion?.bestResult}</span>{" "}
+                  {gamesPlayed?.guessChampion?.bestResult === 1
                     ? "champion"
                     : "champions"}
                 </h3>
@@ -60,62 +66,71 @@ export default function Homepage() {
             </div>
           )}
         </div>
-        <div className="game-container">
+        <div
+          className={`game-container ${
+            !!gamesPlayed ? "games-played-container" : ""
+          }`}
+        >
           <div
             className={`game-link-box ${
-              !!gamesPlayed.memory ? "game-played" : ""
+              !!gamesPlayed?.memory ? "game-played" : ""
             }`}
           >
             <Link to="/champions-memory">Champions Memory</Link>
           </div>
-          {!!gamesPlayed.memory?.bestResult && (
+          {!!gamesPlayed?.memory?.bestResult && (
             <div className="game-results">
               <h2>
-                {gamesPlayed.memory?.gameFinished
+                {gamesPlayed?.memory?.gameFinished
                   ? "You mastered this game!"
                   : "Next time you'll rock!"}
               </h2>
 
-              {gamesPlayed.memory?.gameFinished ? (
+              {gamesPlayed?.memory?.gameFinished ? (
                 <h3>Your memory is prodigious!</h3>
               ) : (
                 <h3>
                   You made it until level:{" "}
-                  <span>{gamesPlayed.memory?.bestResult}</span>
+                  <span>{gamesPlayed?.memory?.bestResult}</span>
                 </h3>
               )}
             </div>
           )}
         </div>
-        <div className="game-container">
+        <div
+          className={`game-container ${
+            !!gamesPlayed ? "games-played-container" : ""
+          }`}
+        >
           <div
             className={`game-link-box ${
-              !!gamesPlayed.typedChampions ? "game-played" : ""
+              !!gamesPlayed?.typedChampions ? "game-played" : ""
             }`}
           >
             <Link to="/type-champions">Type all Champions</Link>
           </div>
-          {!!gamesPlayed.typedChampions?.bestResult && (
+          {!!gamesPlayed?.typedChampions?.bestResult && (
             <div className="game-results">
               <h2>
-                {gamesPlayed.typedChampions?.gameFinished
+                {gamesPlayed?.typedChampions?.gameFinished
                   ? "Your fingers are on fire!"
                   : "Come try it again!"}
               </h2>
 
-              {gamesPlayed.typedChampions?.gameFinished ? (
+              {gamesPlayed?.typedChampions?.gameFinished ? (
                 <h3>
                   <span>
-                    {gamesPlayed.typedChampions?.bestResult?.quantity}
+                    {gamesPlayed?.typedChampions?.bestResult?.quantity}
                   </span>{" "}
-                  in <span>{gamesPlayed.typedChampions?.bestResult?.time}</span>
-                  ! Weren't you Mozart in another life?
+                  in{" "}
+                  <span>{gamesPlayed?.typedChampions?.bestResult?.time}</span>!
+                  Weren't you Mozart in another life?
                 </h3>
               ) : (
                 `You are faster than just ${
-                  gamesPlayed.typedChampions?.bestResult?.quantity
+                  gamesPlayed?.typedChampions?.bestResult?.quantity
                 } ${
-                  gamesPlayed.typedChampions?.bestResult?.quantity === 1
+                  gamesPlayed?.typedChampions?.bestResult?.quantity === 1
                     ? "champion"
                     : "champions"
                 } per minute`
